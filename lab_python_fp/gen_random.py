8e0bb7ce444424a12b5a057448f9300b17f1b8b2
Sinus2x <66667367+Sinus2x@users.noreply.github.com>
2021-12-22 17:24:10 +0300

    Задача 3

---
+++ lab_python_fp/gen_random.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Пример:
+# gen_random(5, 1, 3) должен выдать выдать 5 случайных чисел
+# в диапазоне от 1 до 3, например 2, 2, 3, 2, 1
+# Hint: типовая реализация занимает 2 строки
+from random import randint
+
+def gen_random(num_count, begin, end):
+    for i in range(num_count):
+        yield randint(begin, end)
+
+        
+if __name__ == '__main__':
+    for i in gen_random(10, 23, 57):
+        print(i, end=" ")
